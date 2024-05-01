41d312da9bf51905369200f39870fd37595ec3d1
BUDIDETI PAVANI REDDY <pavanibudideti@gmail.com>
2024-05-01 17:42:44 +0530

    Changed SellerNavBsr.js file

--- src/seller/SellerNavBar.js
+++ src/seller/SellerNavBar.js
@@ -4,7 +4,7 @@ import './seller.css'
 import SellerHome from './SellerHome';
 import SellerProfile from './SellerProfile';
 import AddPet from './AddPet';
-import ViewPets from './ViewPets';
+//import ViewPets from './ViewPets';
 //import ViewPetApplicants from './ViewPetApplicants';
 
 
@@ -30,7 +30,7 @@ const navigate = useNavigate();
             <Link>Pets</Link>
             <div className="dropdown-content">
             <Link to="/addpet">Post a Pet</Link>
-            <Link to="/viewpets">View Pets</Link>
+            {/* <Link to="/viewpets">View Pets</Link> */}
             </div>
     </li>
     {/* <Link to="/viewpetapplicants">Pet Applicants</Link> */}
@@ -42,7 +42,7 @@ const navigate = useNavigate();
          <Route path="/sellerhome" element={<SellerHome/>} exact/>
          <Route path="/sellerprofile" element={<SellerProfile/>} exact/>
          <Route path="/addpet" element={<AddPet/>} exact/>
-         <Route path="/viewpets" element={<ViewPets/>} exact/>
+         {/* <Route path="/viewpets" element={<ViewPets/>} exact/> */}
          {/* <Route path="/viewpetapplicants" element={<ViewPetApplicants/>} exact/> */}
         </Routes>
 
